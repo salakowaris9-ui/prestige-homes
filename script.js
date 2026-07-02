db0499f96e6cb47fe197f1c8e0a8438b3fcd68d8
salakowaris9-ui <salakowaris9@gmail.com>
2026-07-02 07:28:54 +0100

    script.js

--- script.js
+++ script.js
@@ -31,3 +31,33 @@ card.style.transform="translateY(0)";
 });
 
 });
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter=>{
+
+const update=()=>{
+
+const target=+counter.getAttribute('data-target');
+
+const count=+counter.innerText;
+
+const increment=target/80;
+
+if(count<target){
+
+counter.innerText=Math.ceil(count+increment);
+
+setTimeout(update,20);
+
+}else{
+
+counter.innerText=target+"+";
+
+}
+
+}
+
+update();
+
+});
